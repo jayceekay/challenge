9b532dc2d53920a65a3500e4831a201ec45241e4
Justin <>
2022-10-14 17:44:51 -0400

    fixes

--- src/solution.js
+++ src/solution.js
@@ -1,11 +1,31 @@
-class Book {
-  constructor({ isbn, author, sentences }) {
-    this.author = author;
-    this.sentences = sentences;
-    this.isbn = isbn;
+
+class Author {
+  constructor(name) {
+    this.name = name;
+    // don't directly add. improvement: use new ES/js class privacy features
+    this._books = [];
 
     this.left = null;
     this.right = null;
+  }
+
+  addBook(book) {
+    this._books.push(book);
+    // in place but expensive, use a tree
+    this._books.sort((a, b) => a.title < b.title ? -1 : 1);
+  }
+}
+
+class Book {
+  constructor({ isbn, author, title, sentences }) {
+    this.author = author;
+    this.title = title;
+    this.sentences = sentences;
+    this.isbn = isbn;
+
+    // for a future tree
+    // this.left = null;
+    // this.right = null;
   }
 }
 
@@ -14,53 +34,67 @@ class BookTree {
     this.root = null;
   }
 
-  insert(data) {
-    var node = new Book(data);
-
-    if (this.root === null) this.root = node;
-    else this.insertNode(this.root, node);
+  // book is a Book
+  makeAuthorNode(book) {
+    let auth = new Author(book.author)
+    auth.addBook(book)
+    return auth;
   }
 
-  insertNode(node, book) {
-    if (node.author < book.author) {
-      if (node.right === null) node.right = book;
-      else this.insertNode(node.right, book);
+  // data pojo
+  insert(bookData) {
+    var book = new Book(bookData);
+
+    if (this.root === null) this.root = this.makeAuthorNode(book);
+    else this.insertNode(this.root, book);
+  }
+
+  // sloppy passing off of different "types" of data
+  // book is a Book
+  insertNode(authNode, book) {
+    if (authNode == null) { 
+      authNode = this.makeAuthorNode(book)
+      return;
+    }
+
+    if (authNode.name == book.author) {
+      authNode.addBook(book);
+      return;
+    }
+
+    if (authNode.name < book.author) {
+      if (authNode.right === null) authNode.right = this.makeAuthorNode(book);
+      else this.insertNode(authNode.right, book);
     } else {
-      if (node.left === null) node.left = book;
-      else this.insertNode(node.left, book);
+      if (authNode.left === null) authNode.left = this.makeAuthorNode(book);
+      else this.insertNode(authNode.left, book);
     }
   }
 
   print() {
-    return this.walk(this.root, console.log);
+    return this.walk(this.root);
   }
 
-  walk(node, fn) {
-    if (node !== null) {
+  walk(node, fn = console.log) {
+    if (node != null) {
       this.walk(node.left);
+      // may want to use apply?
       fn(node);
       this.walk(node.right);
     }
   }
 }
 
-function titleSort(a, b) {
-  return a.title < b.title;
-}
-
 // for testing. booksArrlooks like this:
 // `{ [isbn]: [auth, title, ...rest], [isbn2]: [auth2, title2, ...rest2] }` for testing purposes
 function organize(booksObj, tree) {
   // o(n)
-  return Object.keys(booksObj).map(isbn => {
-    const [author, title, ...sentences] = booksObj[isbn]
-    //
-    // sort our books by title while we're here - should sort in place?
-    books.sort(titleSort);
+  return Object.keys(booksObj).map((isbn) => {
+    const [author, title, ...sentences] = booksObj[isbn];
 
     // could also use a proper class/struct for this to enforce attrs
     const book = { isbn, author, title, ...sentences };
-    // n log n (max depth based on n items)
+
     return tree.insert(book);
   });
 }
@@ -78,8 +112,7 @@ function processInput(input) {
   return tree;
 }
 
-// for the problem presened in hackerrank with stdio as input
-module.exports.solve = function solve() {
+function solve() {
   process.stdin.resume();
   process.stdin.setEncoding("ascii");
 
@@ -90,9 +123,12 @@ module.exports.solve = function solve() {
   });
 
   process.stdin.on("end", function () {
-    return processInput(input)
+    return processInput(input);
   });
-};
+}
 
+// for the problem presened in hackerrank with stdio as input
+module.exports.solve = solve;
+// for me to test
 module.exports.organize = organize;
 module.exports.BookTree = BookTree;
--- test/test.js
+++ test/test.js
@@ -2,11 +2,14 @@ var expect = require('chai').expect;
 const solution = require('../src/solution')
 const BookTree = solution.BookTree;
 
-describe("solve", () => {
+describe("solve", function() {
+  this.timeout(0);
+
   it("solve", () => {
     const booksObj = require('./test-data')
 
     let tree = new BookTree()
+
     solution.organize(booksObj, tree)
 
     tree.print()
